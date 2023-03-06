d8bc7ad64e67ca13581cd5b3c82a459462db9726
Parth <psp90804@gmail.com>
2023-03-07 01:07:50 +0530

    back to top added

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ function togglenav(){
     if (navbar.style.display === "none") {navbar.style.display = "flex";}
     else {navbar.style.display = "none"}
 }
+
+window.onscroll = function() {scrollfunction()};
+function scrollfunction(){
+    let backtotop = document.getElementsByClassName("backtotop")[1];
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400){
+        backtotop.style.display = "block";
+    } else {
+        backtotop.style.display = "none";
+    }
+}
