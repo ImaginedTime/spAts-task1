d16eaac0723e1ae284bb98f51e068ef90789f291
Parth <psp90804@gmail.com>
2023-03-06 23:33:54 +0530

    hamborgir menu

---
+++ script.js
@@ -0,0 +1,5 @@
+function togglenav(){
+    let navbar = document.getElementById("navbar");
+    if (navbar.style.display === "none") {navbar.style.display = "flex";}
+    else {navbar.style.display = "none"}
+}
